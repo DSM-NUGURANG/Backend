4d76d6f73a1abe7bad1ea1d14154134ef805780d
dohyeon5626 <kodohyeon71@gmail.com>
2022-04-28 19:07:38 +0900

    Add interceptor setting

--- src/main/kotlin/com/dohyeon5626/nugurang/log/LogInterceptor.kt
+++ src/main/kotlin/com/dohyeon5626/nugurang/log/LogInterceptor.kt
@@ -2,24 +2,21 @@ package com.dohyeon5626.nugurang.log
 
 import mu.KLogging
 import org.springframework.web.servlet.HandlerInterceptor
-import org.springframework.web.servlet.ModelAndView
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
 
 class LogInterceptor: HandlerInterceptor {
-
     companion object: KLogging()
 
-    override fun postHandle(
+    override fun afterCompletion(
         request: HttpServletRequest,
         response: HttpServletResponse,
         handler: Any,
-        modelAndView: ModelAndView?
+        exception: Exception?
     ) {
         if (response.status in 400..499)
-            logger.info("[${response.status}] - ${request.method} / ${request.requestURI} / ${request.remoteUser}")
+            logger.info("[${response.status}] - ${request.method} | ${request.requestURI} | ${request.getHeader("User-Agent")}")
         else if (response.status >= 500)
-            logger.error("[${response.status}] - ${request.method} / ${request.requestURI} / ${request.remoteUser}")
+            logger.error("[${response.status}] - ${request.method} | ${request.requestURI} | ${request.getHeader("User-Agent")}")
     }
-
 }
--- src/main/kotlin/com/dohyeon5626/nugurang/security/WebConfig.kt
+++ src/main/kotlin/com/dohyeon5626/nugurang/security/WebConfig.kt
@@ -1,7 +1,9 @@
 package com.dohyeon5626.nugurang.security
 
+import com.dohyeon5626.nugurang.log.LogInterceptor
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
@@ -11,5 +13,10 @@ class WebConfig : WebMvcConfigurer {
             .allowedOrigins("*")
             .allowedMethods("*")
             .allowedHeaders("*")
+    }
+
+    override fun addInterceptors(registry: InterceptorRegistry) {
+        registry.addInterceptor(LogInterceptor())
+            .addPathPatterns("/*")
     }
 }
